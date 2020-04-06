e29e27806bb137120076f3335bb0e1c3f4b54153
Wes Ring <WesR@users.noreply.github.com>
2020-04-06 00:00:50 -0400

    Create you-dl.sh

---
+++ you-dl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Used to archive youtube playlists 
+# Set the log name, download location, and Playlist link
+/usr/local/bin/youtube-dl --download-archive "download.log" -i --add-metadata --all-subs --embed-subs -f "(bestvideo[vcodec^=av01][height>=1080][fps>30]/bestvideo[vcodec=vp9.2][height>=1080][fps>30]/bestvideo[vcodec=vp9][height>=1080][fps>30]/bestvideo[vcodec^=av01][height>=1080]/bestvideo[vcodec=vp9.2][height>=1080]/bestvideo[vcodec=vp9][height>=1080]/bestvideo[height>=1080]/bestvideo[vcodec^=av01][height>=720][fps>30]/bestvideo[vcodec=vp9.2][height>=720][fps>30]/bestvideo[vcodec=vp9][height>=720][fps>30]/bestvideo[vcodec^=av01][height>=720]/bestvideo[vcodec=vp9.2][height>=720]/bestvideo[vcodec=vp9][height>=720]/bestvideo[height>=720]/bestvideo)+(bestaudio[acodec=opus]/bestaudio)/best" --merge-output-format mkv -o "/path/to/storage/%(playlist_index)s - %(title)s - %(id)s.%(ext)s" "YouTube Playlist Link"
